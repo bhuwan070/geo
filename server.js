91346395ae9a2f7f69571e2c3a4a77d97d25b9b8
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-02-04 23:20:14 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./route/authRoutes");
+
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+
+connectDB();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server listening on port${PORT}`));
